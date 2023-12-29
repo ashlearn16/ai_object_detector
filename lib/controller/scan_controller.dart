abba50025a9e51c451d23eb5fae2c4b10073ed94
devashpontay <devashpontay@gmail.com>
2023-12-29 19:21:43 +0800

    IC

---
+++ lib/controller/scan_controller.dart
@@ -0,0 +1,79 @@
+import 'package:camera/camera.dart';
+import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:tflite_v2/tflite_v2.dart';
+
+class ScanController extends GetxController {
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    initCamera();
+    initTFLite();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    cameraController.dispose();
+  }
+
+  late CameraController cameraController;
+  late List<CameraDescription> cameras;
+
+  var isCameraInitialized = false.obs;
+  var cameraCount = 0;
+
+  initCamera() async {
+    if (await Permission.camera.request().isGranted) {
+      cameras = await availableCameras();
+
+      cameraController = CameraController(cameras[0], ResolutionPreset.max);
+      await cameraController.initialize().then((value) {
+        cameraController.startImageStream((image) {
+          cameraCount++;
+          if (cameraCount % 40 == 0) {
+            cameraCount = 0;
+            objectDetector(image);
+          }
+          update();
+        });
+      });
+      isCameraInitialized(true);
+      update();
+    } else {
+      print("Permission denied");
+    }
+  }
+
+  initTFLite() async {
+    await Tflite.loadModel(
+      model: "assets/gising-kapatid-test.tflite",
+      labels: "assets/labels-gk.txt",
+      isAsset: true,
+      numThreads: 1,
+      useGpuDelegate: false,
+    );
+  }
+
+  objectDetector(CameraImage image) async {
+    var detector = await Tflite.runModelOnFrame(
+      bytesList: image.planes.map((e) {
+        return e.bytes;
+      }).toList(),
+      asynch: true,
+      imageHeight: image.height,
+      imageWidth: image.width,
+      imageMean: 127.5,
+      imageStd: 127.5,
+      numResults: 1,
+      rotation: 90,
+      threshold: 0.4,
+    );
+
+    if (detector != null) {
+      print("Result is $detector");
+    }
+  }
+}
